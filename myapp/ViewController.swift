ea9e35ee859225c7c5d04d596a79bd5fc18f091c
Alvian <alvigoza@gmail.com>
2020-03-05 14:11:57 +0700

    Initial Commit

---
+++ myapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myapp
+//
+//  Created by Alvian Gozali on 05/03/20.
+//  Copyright © 2020 Alvian Gozali. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
